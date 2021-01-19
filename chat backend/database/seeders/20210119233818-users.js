34be449a3617300fd3f463fed45fc113e76a1cdf
Daniel151505 <daniel150520011@gmail.com>
2021-01-19 18:52:10 -0500

    Creación del archivo user seeder

---
+++ chat backend/database/seeders/20210119233818-users.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+      await queryInterface.bulkInsert('Users',[
+        {
+        firstName:'John',
+        lastname:'Dario',
+        email: 'john.doe@gmail.com',
+        password:'secret',
+        gender:'male'
+        },
+        {
+          firstName:'Daniel',
+          lastname:'Altamirano',
+          email: 'daniel.c@gmail.com',
+          password:'secret',
+          gender:'male'
+          },
+          {
+            firstName:'Sara',
+            lastname:'Connor',
+            email: 'saraconn@gmail.com',
+            password:'secret',
+            gender:'female'
+            }
+      ])
+
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
