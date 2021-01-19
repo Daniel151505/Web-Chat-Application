6db5769df5328715aa7531265669ea3cff45a779
Daniel151505 <daniel150520011@gmail.com>
2021-01-18 21:07:59 -0500

    Instalando nodemon

--- chat backend/index.js
+++ chat backend/index.js
@@ -3,8 +3,8 @@ const app= express()
 
 const port=3000
 
-app.get('/home',(req,res) =>{
-   return res.send('Home Screen')
+app.get('/login',(req,res) =>{
+   return res.send('login Screen works now')
 } )
 
 app.listen(port, () => {
