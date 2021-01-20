0e1ae414d7ab16ce6aecfbeb5ab361d3cecf6175
Daniel151505 <daniel150520011@gmail.com>
2021-01-20 15:16:30 -0500

    Parsing request Body

--- chat backend/index.js
+++ chat backend/index.js
@@ -4,9 +4,16 @@ const config = require ('./config/app')
 
 const router = require('./router')
 
+const bodyParser = require('body-parser')
+
 const app= express()
 
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
 app.use(router)
+
+
 
 const port= config.addPort
 
