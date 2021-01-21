34e643f89333e12e9815d137ee1b7fab83e02c5d
Daniel151505 <daniel150520011@gmail.com>
2021-01-21 16:28:11 -0500

    Adding server side form request validation

--- chat backend/controllers/authController.js
+++ chat backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User= require('../models').User
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const {validationResult} = require ("express-validator")
 
 exports.login = async (req,res) => {
     const{email, password} = req.body
