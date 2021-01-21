bf1fffc763151a4aee1f7be10ee22da663fb2b98
Daniel151505 <daniel150520011@gmail.com>
2021-01-20 22:37:00 -0500

    Finishing register functionality and creating proper secret key

--- chat backend/controllers/authController.js
+++ chat backend/controllers/authController.js
@@ -3,9 +3,12 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 exports.login = async (req,res) => {
-    const{email, password} = req.body;
+    const{email, password} = req.body
 
     try {
+
+        const secret = require('crypto').randomBytes(64).toString('hex')
+
         // find the user
         const user = await User.findOne({
             where:{
@@ -31,13 +34,22 @@ exports.login = async (req,res) => {
 }
 exports.register = async (req,res) => {
 
+    try{
+        const user = await User.create(req.body)
+
+        const userWithToken = generateToken(user.get({raw : true}))
+        return res.send(userWithToken)
+    }catch(e){
+        return res.status(500).json({message: e.message})
+    }
+
+
+
 }
 
 const generateToken =(user) => {
-
-    console.log(user)
     
-    delete user.password
+    //delete user.password
 
     const token = jwt.sign(user, 'secret' , {expiresIn: 86400})
 
