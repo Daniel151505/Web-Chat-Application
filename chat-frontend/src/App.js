3067a8c83c377d1f3f98f4a77934db1c624a3139
Daniel151505 <daniel150520011@gmail.com>
2021-01-21 21:03:38 -0500

    Adding import

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -3,7 +3,7 @@ import Register from './components/Auth/Register'
 import Chat from './components/Chat/Chat'
 import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
 
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
