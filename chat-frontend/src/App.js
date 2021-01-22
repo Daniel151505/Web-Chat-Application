af2932f45d10bbeb4c29ace498f4d81d8ec7de20
Daniel151505 <daniel150520011@gmail.com>
2021-01-21 19:08:32 -0500

    Creating initial components and adding React Router

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -1,11 +1,22 @@
+import Login from './components/Auth/Login'
+import Register from './components/Auth/Register'
+import Chat from './components/Chat/Chat'
+import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
 
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>Chat app</h1>
+      <Switch>
+      <Route exact path ='/' component={Chat}/>
+      <Route path ='/login' component={Login}/>
+      <Route path ='/register' component={Register}/>
+      <Route render={() =><h1>404 page not found</h1>}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
