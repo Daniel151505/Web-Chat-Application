f8d2c8000bf4295b584ec050ba15d1cec13a3dde
Daniel151505 <daniel150520011@gmail.com>
2021-01-21 19:19:57 -0500

    Creating login component

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import loginImage from '../../assets/images/login.svg'
 
 const Login = () => {
     return (
-        <h1>Login Screen</h1>
+        <div id='auth-container'>
+            <div id ='auth-card'>
+                <div>
+                    <div id='image-section'>
+                        <img src={loginImage} alt='Login' />
+                    </div>
+                    <div id='form-section'>
+                        <h2>Wellcome back</h2>
+
+                        <form>
+                            <div className='input-field'>
+                                <input placeholder='Email' />
+                            </div>
+
+                            <div className='input-field'>
+                                <input placeholder='Password' />
+                            </div>
+
+                            <button>Login</button>
+                        </form>
+
+                        <p>Don´t have an account? Register</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
