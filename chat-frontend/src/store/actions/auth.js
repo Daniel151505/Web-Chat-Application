e786ff6a97bef17388baa1845367f298a75eb648
Daniel151505 <daniel150520011@gmail.com>
2021-01-23 18:42:09 -0500

    Creating Redux store

--- chat-frontend/src/store/actions/auth.js
+++ chat-frontend/src/store/actions/auth.js
@@ -1,11 +1,12 @@
-export const LOGIN = 'LOGIN'
 import AuthService from '../../services/authService'
+export const LOGIN = 'LOGIN'
+
 
 export const login = (params) => dispatch => {
 
     return AuthService.login(params)
     .then(data => {
-        console.log(res)
+        console.log(data)
         dispatch({type: LOGIN, payload: data })
     })
 
