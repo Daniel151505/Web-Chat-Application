66a76d0927d53843e9c4b3cde05bc208ed5c9a81
Daniel151505 <daniel150520011@gmail.com>
2021-01-24 12:26:15 -0500

    Fix error

--- chat-frontend/src/components/Auth/Login.js
+++ chat-frontend/src/components/Auth/Login.js
@@ -1,64 +1,58 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import loginImage from '../../assets/images/login.svg'
-import {Link} from 'react-router-dom'
-import AuthService from '../../services/authService'
+import { Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { login } from '../../store/actions/auth'
+
 import './Auth.scss'
 
-const Login = () => {
+const Login = ({ history }) => {
 
-    const [ email, setEmail]=useState('john.doe@gmail.com')
-    const [password,setPassword]=useState('secret')
+    const dispatch = useDispatch()
 
-    const submitForm= (e) =>{
+    const [email, setEmail] = useState('john.doe@gmail.com')
+    const [password, setPassword] = useState('secret')
+
+    const submitForm = (e) => {
         e.preventDefault()
 
-        AuthService.login({email,password}).then(res => console.log(res))
-
-      /*  axios.post('http://127.0.0.1:3000/login', {email,password})
-            .then(res => {
-                console.log("res",res);
-            })
-            .catch(err => {
-                console.log('err',err);
-            }) 
-       */
-        console.log({email,password});
+        dispatch(login({ email, password }, history))
     }
 
     return (
         <div id='auth-container'>
-            <div id ='auth-card'>
-                <div className='car-shadow'>
+            <div id='auth-card'>
+                <div className='card-shadow'>
                     <div id='image-section'>
                         <img src={loginImage} alt='Login' />
                     </div>
+
                     <div id='form-section'>
-                        <h2>Wellcome back</h2>
+                        <h2>Welcome back</h2>
 
                         <form onSubmit={submitForm}>
                             <div className='input-field mb-1'>
-                                <input 
-                                onChange={e => setEmail(e.target.value)}
-                                value={email}
-                                required='required'
-                                type='text'
-                                placeholder='Correo' />
+                                <input
+                                    onChange={e => setEmail(e.target.value)}
+                                    value={email}
+                                    required='required'
+                                    type='text'
+                                    placeholder='Email' />
                             </div>
 
                             <div className='input-field mb-2'>
-                                <input 
-                                onChange={e => setPassword(e.target.value)}
-                                value={password}
-                                required='required'
-                                type='password'
-                                placeholder='Password' />
+                                <input
+                                    onChange={e => setPassword(e.target.value)}
+                                    value={password}
+                                    required='required'
+                                    type='password'
+                                    placeholder='Password' />
                             </div>
 
-                            <button>Login</button>
+                            <button>LOGIN</button>
                         </form>
 
-                        <p>No tienes una cuenta? <Link to='/register'>Registrate</Link></p>
-
+                        <p>Don't have an account? <Link to='/register'>Register</Link></p>
                     </div>
                 </div>
             </div>
